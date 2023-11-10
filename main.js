ee02042ca715e74e5c258672578de573daf98dc1
Luis Monjardin <lfmonguer@gmail.com>
2023-11-10 14:37:40 -0600

    Almost done, added Login into Todoist using direct url, and tested clicking Add task button and filling it, pending using the parsed task from Trello

--- main.js
+++ main.js
@@ -1,43 +1,122 @@
-import puppeteer from 'puppeteer';
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch({
+  // Part 1: Get tasks from Trello
+  const trelloBrowser = await puppeteer.launch({
     headless: "new",
   });
-  const page = await browser.newPage();
-  await page.goto('https://trello.com/b/QvHVksDa/personal-work-goals');
-  
-  const [button_okay] = await page.$x("//button[contains(., 'Okay, got it')]");
-  if (button_okay) {
-      console.log("Okay button founded")
-      await button_okay.click();
-      console.log(" Okay button clicked");
-    } 
-    /*
-// Example: Extract task titles using XPath-like selector
-  const cardTitles = await page.$$eval('[data-testid^="list-header"]', (cards) => {
-        return cards.map((card) => card.textContent.trim());
-    });  
-  */
- 
-  
-    // Select all list headers
-  const listHeaders = await page.$$('[data-testid^="list-header"]');
+  const trelloPage = await trelloBrowser.newPage();
+  await trelloPage.goto('https://trello.com/b/QvHVksDa/personal-work-goals');
 
-  const tasks = [];
-  for (const listHeader of listHeaders) {
-       const title = await listHeader.evaluate((el) => el.textContent.trim());
-       //console.log(title);
-       // Find card names within the same list
-       const cardNames = await listHeader.$$('[data-testid^="card-name"]');
-       const content = await Promise.all(cardNames.map((cardName) => cardName.evaluate((el) => el.textContent.trim())));
+  const [buttonOkay] = await trelloPage.$x("//button[contains(., 'Okay, got it')]");
+  if (buttonOkay) {
+    await buttonOkay.click();
+  }
 
-       tasks.push({ title, content });
-    }   
-    
-  await page.screenshot({path: './test.png'}); 
-  await browser.close();
-  console.log('Tasks:', tasks);
-  //console.log('Task titles:', cardTitles);
-  console.log("Done");
-  })();
+  const trelloTasks = await trelloPage.evaluate(() => {
+    const listHeaders = document.querySelectorAll('[data-testid^="list-header"]');
+    const tasks = [];
+
+    for (let i = 0; i < listHeaders.length; i++) {
+      const listHeader = listHeaders[i];
+      const title = listHeader.textContent.trim();
+
+      const cardNames = listHeader.parentElement.querySelectorAll('[data-testid^="card-name"]');
+      const content = Array.from(cardNames).map((cardName) => cardName.textContent.trim());
+
+      tasks.push({ title, content });
+    }
+
+    return tasks;
+  });
+
+  // Randomly select one line from content for each of the 5 tasks
+  const selectedTasks = [];
+  for (let i = 0; i < 5; i++) {
+    const randomIndex = Math.floor(Math.random() * trelloTasks.length);
+    const randomTask = {
+      title: trelloTasks[randomIndex].title,
+      content: [trelloTasks[randomIndex].content[Math.floor(Math.random() * trelloTasks[randomIndex].content.length)]],
+    };
+    selectedTasks.push(randomTask);
+    trelloTasks.splice(randomIndex, 1); // Remove the selected task to avoid duplicates
+  }
+
+  // Only for debug
+  console.log('Selected Tasks:', selectedTasks);
+
+  // Save selected tasks to a JSON file
+  const jsonSelectedTasks = JSON.stringify(selectedTasks, null, 2);
+  fs.writeFileSync('selectedTasks.json', jsonSelectedTasks, 'utf-8');
+
+  // Close Trello browser
+  await trelloBrowser.close();
+
+  // Part 2: Login to Todoist
+  const todoistBrowser = await puppeteer.launch({
+    headless: "new",
+  });
+  const todoistPage = await todoistBrowser.newPage();
+  await todoistPage.setViewport({ width: 1200, height: 800 });
+
+  // Navigate to Todoist
+  await todoistPage.goto('https://todoist.com/auth/login');
+
+  // Find and fill the email input field
+  const emailInputSelector = 'input[placeholder="Enter your email..."]';
+  await todoistPage.waitForSelector(emailInputSelector);
+
+  // Only for debug
+  await todoistPage.screenshot({path: './1-Login_loadedPage.png'});
+
+  const userEmail = 'root.lfmonguer@gmail.com'; 
+  await todoistPage.type(emailInputSelector, userEmail);
+
+  // Find and fill the password input field
+  const passwordInputSelector = 'input[placeholder="Enter your password..."]'; 
+  const userPassword = 'devChallenge1'; 
+  await todoistPage.type(passwordInputSelector, userPassword);
+
+  // Only for debug
+  await todoistPage.screenshot({path: './2-Login_filled.png'});
+
+  // You can also submit the form if needed
+  await todoistPage.keyboard.press('Enter');  
+
+  // Wait for navigation
+  await todoistPage.waitForNavigation();
+
+  // Wait until the button is visible
+  const buttonSelector = '#quick_find';
+  await todoistPage.waitForSelector(buttonSelector, { visible: true });
+
+  // Only for debug
+  await todoistPage.screenshot({path: './3-Login_success.png'});
+
+  const buttonElement = await todoistPage.$('button[data-add-task-navigation-element="true"]');
+
+  if (buttonElement) {
+    await buttonElement.click();
+    console.log("clicked")
+  }
+
+  // Only for debug
+  await todoistPage.screenshot({path: './4-AddButtonPress_success.png'});
+
+  // Find the contenteditable div based on your structure
+  const contentEditableSelector = 'div[contenteditable="true"][data-typist-editor="true"][role="textbox"][aria-readonly="false"][aria-multiline="true"][aria-label="Task name"]';
+  const contentEditableElement = await todoistPage.waitForSelector(contentEditableSelector);
+
+  // Click on the contenteditable element to focus it
+  await contentEditableElement.click();
+
+  // Type your desired text into the focused contenteditable element
+  await todoistPage.keyboard.type('Task Test');
+
+  // Only for debug
+  await todoistPage.screenshot({path: './5-AddButtonFilled_success.png'});
+
+  // Close the Todoist browser
+  await todoistBrowser.close();
+})();
